2901b6414efbabde0dab630c0bbc171d583544bb
matlab670 <46998280+matlab670@users.noreply.github.com>
2023-01-22 13:34:59 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: '48f0098a-976c-4813-b7f5-48400d07127b',
+  obfuscatedClientSecret: 'U2FsdGVkX18nL/BzR6WbPZVW+N37Csqw6EPFFVFdnhnnVSSzCr9SwtCEI8svGA7QGlDqw68OHG8G//fl4X5Ghw==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
